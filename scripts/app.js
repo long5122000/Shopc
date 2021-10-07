c0a2eb098c2c692aff8c5d33169f36e59ebbf58c
joomaio.longld <longluong2x@gmail.com>
2021-10-07 10:10:15 +0700

    update source code

--- scripts/app.js
+++ scripts/app.js
@@ -66,21 +66,10 @@ let HomeSwiper = new Swiper(".home-swiper", {
     }
     window.addEventListener('scroll',scrollActive);
 
-//   const sections = document.querySelectorAll('section[id]')
+/* scrollup */
 
-//   function scrollActive(){
-//       const scrollY = window.pageYOffset
-  
-//       sections.forEach(current =>{
-//           const sectionHeight = current.offsetHeight,
-//                 sectionTop = current.offsetTop - 50,
-//                 sectionId = current.getAttribute('id')
-  
-//           if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//               document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//           }else{
-//               document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//           }
-//       })
-//   }
-//   window.addEventListener('scroll', scrollActive)
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll'); 
+}
+window.addEventListener('scroll' , scrollUp)
